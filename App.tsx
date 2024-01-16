e6f80c87e4543b757b202a090bccb63c044f15aa
Luiz Henrique <luizhjramos@outlook.com>
2024-01-15 23:46:18 -0300

    refactor: initial project structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GroupsScreen } from "./src/screens/Groups";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <GroupsScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
