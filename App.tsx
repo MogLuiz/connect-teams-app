7e57443623dad628cdbc28615a74bdc606b1a1c9
Luiz Henrique <luizhjramos@outlook.com>
2024-01-23 00:02:42 -0300

    feat: apply roboto font on app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { ActivityIndicator } from "react-native";
 import { GroupsScreen } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import theme from "@theme/.";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <GroupsScreen />
+      {fontsLoaded ? <GroupsScreen /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
