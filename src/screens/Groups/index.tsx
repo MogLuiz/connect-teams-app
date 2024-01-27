58fa550800fc0d5c44111b6c4414f77b3c8fdd36
Luiz Henrique <luizhjramos@outlook.com>
2024-01-26 23:44:22 -0300

    feat: add GroupCard list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import * as S from "./styles";
 import { GroupCard } from "@components/GroupCard";
 
-export const GroupsScreen = () => (
-  <S.Container>
-    <Header hasBackButton />
-    <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-    <GroupCard title="Galera do Ignite" />
-  </S.Container>
-);
+export const GroupsScreen = () => {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+
+  return (
+    <S.Container>
+      <Header hasBackButton />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
+    </S.Container>
+  );
+};
